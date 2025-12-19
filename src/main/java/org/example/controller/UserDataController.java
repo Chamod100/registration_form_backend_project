6badeda12706f081e1d9b7c920a1b3b973cf0869
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-19 11:11:53 +0530

    complete project.       factor UserDataController: fix package name, update API path, and improve constructor injection

--- src/main/java/org/example/controller/UserDataController.java
+++ src/main/java/org/example/controller/UserDataController.java
@@ -1,4 +1,4 @@
-package org.example.controoler;
+package org.example.controller;
 
 import org.example.Model.Entity.UserData;
 import org.example.service.UserDataService;
@@ -6,14 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/Api")
+@RequestMapping("/api")
 public class UserDataController {
 
+    private final UserDataService userDataService;
+
     @Autowired
-    private UserDataService userDataService;
+    public UserDataController(UserDataService userDataService) {
+        this.userDataService = userDataService;
+    }
 
     @PostMapping("/saveData")
-    @CrossOrigin(origins = "http://http://127.0.0.1:5500/")
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     public UserData saveData(@RequestBody UserData userData) {
         return userDataService.saveUserData(userData);
     }
